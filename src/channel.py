3b06438578f97b45857659229f5cc190d6f48341
RomanZemlin <roman.zemlin@mail.ru>
2023-06-26 19:03:37 +0300

    Модифицирован конструктор Channel, добавлен класс-метод get-servise и метод to_json, код в файле выдает ожидаемые значения

--- src/channel.py
+++ src/channel.py
@@ -1,17 +1,42 @@
 import os
+import json
 from googleapiclient.discovery import build
 
 
 class Channel:
     """Класс для ютуб-канала"""
+    api_key: str = os.getenv('API_KEY')
+    __youtube = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
+        self.channel = self.__youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        self.title = self.channel['items'][0]['snippet']['title']
+        self.video_count = int(self.channel['items'][0]['statistics']['videoCount'])
+        self.url = f"https://www.youtube.com/channel/{channel_id}"
+        self.description = self.channel['items'][0]['snippet']['description']
+        self.subscribers_count = int(self.channel['items'][0]['statistics']['subscriberCount'])
+        self.view_count = int(self.channel['items'][0]['statistics']['viewCount'])
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        api_key: str = os.getenv('API_KEY')
-        youtube = build('youtube', 'v3', developerKey=api_key)
-        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
-        print(channel)
+        print(json.dumps(self.channel, indent=2, ensure_ascii=False))
+
+    def to_json(self, filename):
+        data = {
+            "channel_id": self.channel_id,
+            "title": self.title,
+            "description": self.description,
+            "url": self.url,
+            "subscribers_count": self.subscribers_count,
+            "video_count": self.video_count,
+            "view_count": self.view_count
+        }
+        with open(filename, 'w') as file:
+            json.dump(data, file, indent=4)
+
+    @classmethod
+    def get_service(cls):
+        return cls.__youtube
+
