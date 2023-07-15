13c57a157ac8b592bb1682fcfcfa8e03ef878ab3
RomanZemlin <roman.zemlin@mail.ru>
2023-07-16 00:40:49 +0300

    При передаче неправильно id видео, инициализируется свойство video_id и остальные значения принимают None

--- src/video.py
+++ src/video.py
@@ -1,19 +1,56 @@
 from src.channel import Channel
+import os
+from googleapiclient.discovery import build
 
 
-class Video(Channel):
+class Video:
 
-    def __init__(self, video_id):
-        video_response = super().get_service().videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                                             id=video_id).execute()
-        self.video_id = video_id
-        self.video_title = video_response['items'][0]['snippet']['title']
-        self.video_url = f"https://youtu.be/{video_response['items'][0]['id']}"
-        self.view_count = video_response['items'][0]['statistics']['viewCount']
-        self.like_count = video_response['items'][0]['statistics']['likeCount']
+    __API_KEY: str = os.getenv('API_KEY')
+    __youtube = build('youtube', 'v3', developerKey=__API_KEY)
 
-    def __str__(self):
-        return self.video_title
+    def __init__(self, video_id: str):
+        self.__video_id = video_id
+        if self.__verify_video_id(video_id) != 0:
+
+            self.__video_response = self.__youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                                 id=self.__video_id).execute()
+            self.__title = self.__video_response['items'][0]['snippet']['title']
+            self.__video_url = f"https://www.youtube.com/watch?v={self.__video_id}"
+            self.__view_count = self.__video_response['items'][0]['statistics']['viewCount']
+            self.__like_count = self.__video_response['items'][0]['statistics']['likeCount']
+        else:
+            self.__title = None
+            self.__video_url = None
+            self.__view_count = None
+            self.__like_count = None
+
+    def __str__(self) -> str:
+        return self.__title
+
+    def __verify_video_id(self, video_id):
+        video_response = self.__youtube.videos().list(part='status', id=video_id).execute()
+        search_count = video_response['pageInfo']['totalResults']
+        return search_count
+
+    @property
+    def video_id(self) -> str:
+        return self.__video_id
+
+    @property
+    def title(self) -> str:
+        return self.__title
+
+    @property
+    def video_url(self) -> str:
+        return self.__video_url
+
+    @property
+    def view_count(self) -> str:
+        return self.__view_count
+
+    @property
+    def like_count(self) -> str:
+        return self.__like_count
 
 
 class PLVideo(Video):
